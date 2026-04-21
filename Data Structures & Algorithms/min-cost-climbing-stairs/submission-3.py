d9814e73230fc35cd66889c4b890490fed4326f9
Shubeeksh K <94282846+beeksh@users.noreply.github.com>
2026-04-21 14:07:21 +0530

    Add: min-cost-climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-3.py
@@ -0,0 +1,20 @@
+# class Solution:
+#     def minCostClimbingStairs(self, cost: List[int]) -> int:
+#         d = {}
+#         def x(n):
+#             if n>=len(cost):
+#                 return 0
+#             if n in d:
+#                 return d[n]
+#             else:
+#                 d[n] = cost[n]+min(x(n+1), x(n+2))
+#             return d[n]
+#         return min(x(0),x(1))
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        n = len(cost)
+        d = [0]*(n+2)
+        for i in range(n-1,-1,-1):
+            d[i] = cost[i]+min(d[i+1], d[i+2])
+        return min(d[i], d[i+1])
